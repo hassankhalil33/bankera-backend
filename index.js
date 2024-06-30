44e68135fd4cb4876fe791e93e07f348c727013c
hassankhalil33 <Hassan.Khalil.33@Hotmail.com>
2024-06-30 23:04:22 +0300

    Created Server Service

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("potato");
+const express = require("express");
+require("dotenv").config();
+
+const serverPort = process.env.SERVER_PORT;
+const app = express();
+
+app.listen(serverPort, () => {
+  console.log (`Running On Port: ${serverPort}`);
+});
