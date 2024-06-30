23abecd90307d9e0a27aec1f2213a30d7f94e89a
hassankhalil33 <Hassan.Khalil.33@Hotmail.com>
2024-07-01 02:04:47 +0300

    Adjusted DB URL

--- config/db.config.js
+++ config/db.config.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose")
-const DB_URL = `mongodb+srv://admin:${process.env.DB_PASS}@cluster0.qfgtqmn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+const DB_URL = `mongodb+srv://admin:${process.env.DB_PASS}@cluster0.qfgtqmn.mongodb.net/bankera?retryWrites=true&w=majority&appName=Cluster0`
 
 mongoose.connect(DB_URL)
 .then(() => {
