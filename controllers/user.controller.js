87e177b8396bf8738f03817935b98dfd927428fc
hassankhalil33 <Hassan.Khalil.33@Hotmail.com>
2024-07-04 00:27:47 +0300

    Added Error Handling in User Controller

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -6,9 +6,15 @@ const getUser = async (req, res) => {
 
   const user = await User.findOne({
     username: username
-  });
+  })
+  .populate("accounts")
+  .exec();
 
-  res.json(user);
+  if(user) {
+    res.json(user);
+  } else {
+    res.status(400).send({ message: "User Doesnt Exist" });
+  };
 };
 
 const createUser = async (req, res) => {
@@ -19,17 +25,29 @@ const createUser = async (req, res) => {
   user.password = password;
   user.email = email;
 
-  await user.save();
+  await user.save()
+    .catch((err) => {
+      console.log(err);
+      res.send(400, "Bad Request");
+    });
 
   const account = new Account;
   account.user_id = user._id;
 
-  await account.save();
+  await account.save()
+    .catch((err) => {
+      console.log(err);
+      res.send(400, "Bad Request");
+  });
 
   user.account_count = user.account_count + 1;
   user.accounts = [...user.accounts, account._id];
 
-  await user.save();
+  await user.save()
+    .catch((err) => {
+      console.log(err);
+      res.send(400, "Bad Request");
+  });
 
   res.json({message: "user created successfully"});
 };
